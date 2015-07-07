320f562149ec24543dfba9d9d854ddb5be519576
gmp26 <gmp26@cam.ac.uk>
2015-07-07 17:35:26 +0100

    datascript experiments

--- src/db/squares.cljs
+++ src/db/squares.cljs
@@ -34,22 +34,48 @@
                                        (for [dy (range 0 (- n2 y dx))]
                                          (square x y dx dy)))))))))))
 
-
 ;;
 ;; square db
 ;;
 ;; Implicit join, multi-valued attribute
 
+(def conn 
+  (d/create-conn 
+   {:p {:db/cardinality :db.cardinality/many}}
+))
+
+(let [squares (all-squares 3)]
+  (d/transact! conn 
+               (map-indexed (fn [ix sq]
+                              {
+                               :color :red
+                               :name ix
+                               :p sq})
+                            squares))
+  (prn (d/q '[:find (aggregate ?p) ?p ?color
+              :where
+              [?e :color ?color]
+              [?e :p ?p]
+              ]
+            @conn))
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; sample db stuff ;;;;;;;;;;;
+
+
+;; reference
+
+
 (let [schema {:aka {:db/cardinality :db.cardinality/many}}
       conn   (d/create-conn schema)]
   (d/transact! conn [ { :db/id -1
-                        :name  "Maksim"
-                        :age   45
-                        :aka   ["Maks Otto von Stirlitz", "Jack Ryan"] } ])
+                       :name  "Maksim"
+                       :age   45
+                       :aka   ["Maks Otto von Stirlitz", "Jack Ryan"] } ])
   (d/q '[ :find  ?n ?a
-          :where [?e :aka "Maks Otto von Stirlitz"]
-                 [?e :name ?n]
-                 [?e :age  ?a] ]
+         :where [?e :aka "Maks Otto von Stirlitz"]
+         [?e :name ?n]
+         [?e :age  ?a] ]
        @conn))
 
 ;; => #{ ["Maksim" 45] }
@@ -57,12 +83,12 @@
 
 ;; Destructuring, function call, predicate call, query over collection
 
-(defn test [] (d/q '[ :find  ?k ?x
-                     :in    [[?k [?min ?max]] ...] ?range
-                     :where [(?range ?min ?max) [?x ...]]
-                     [(even? ?x)] ]
-                   { :a [1 7], :b [2 4] }
-                   range))
+(defn testdb [] (d/q '[ :find  ?k ?x
+                       :in    [[?k [?min ?max]] ...] ?range
+                       :where [(?range ?min ?max) [?x ...]]
+                       [(even? ?x)] ]
+                     { :a [1 7], :b [2 4] }
+                     range))
 
 ;; => #{ [:a 2] [:a 4] [:a 6] [:b 2] }
 
@@ -70,11 +96,11 @@
 ;; Recursive rule
 
 (d/q '[ :find  ?u1 ?u2
-        :in    $ %
-        :where (follows ?u1 ?u2) ]
-      [ [1 :follows 2]
-        [2 :follows 3]
-        [3 :follows 4] ]
+       :in    $ %
+       :where (follows ?u1 ?u2) ]
+     [ [1 :follows 2]
+       [2 :follows 3]
+       [3 :follows 4] ]
      '[ [(follows ?e1 ?e2)
          [?e1 :follows ?e2]]
         [(follows ?e1 ?e2)
@@ -88,11 +114,16 @@
 
 ;; Aggregates
 
-(d/q '[ :find ?color (max ?amount ?x) (min ?amount ?x)
-        :in   [[?color ?x]] ?amount ]
-     [[:red 10]  [:red 20] [:red 30] [:red 40] [:red 50]
-      [:blue 7] [:blue 8]]
-     3)
+(prn (d/q '[ :find ?color (max ?amount ?x) (min ?amount ?x)
+            :in   [[?color ?x]] ?amount ]
+          [[:red 10]  [:red 20] [:red 30] [:red 40] [:red 50]
+           [:blue 7] [:blue 8]]
+          1))
 
 ;; => [[:red  [30 40 50] [10 20 30]]
 ;;     [:blue [7 8] [7 8]]]
+
+
+;; end commented region
+
+
